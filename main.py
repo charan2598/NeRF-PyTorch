1700cf728a9e5c94d6b391d3fd31473539bcbb7f
Charan Kumar <charan2598@gmail.com>
2025-02-27 18:41:18 -0800

    Minor changes after fix to dataloader code.

---
+++ main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import torch.optim as optim
+from model import NeRF_model
+from dataloader import BlenderDataset
+
+# 
+basedir = r"D:\CV_Projects\NeRF-PyTorch\data\lego"
+
+# Define Datasets
+train_dataset, train_render_poses, train_data_intrinsics = BlenderDataset.create_and_return_object(basedir=basedir, split="train", resolution=400)
+val_dataset, val_render_poses, val_data_intrinsics  = BlenderDataset.create_and_return_object(basedir=basedir, split="val", resolution=400)
+test_dataset, test_render_poses, test_data_intrinsics  = BlenderDataset.create_and_return_object(basedir=basedir, split="test", resolution=400)
+
+height, width, focal_length = train_data_intrinsics
+camera_intrinsic_matrix = np.array([
+    [focal_length, 0, 0.5*width],
+    [0, focal_length, 0.5*height],
+    [0, 0, 1]
+])
+
+# Define Dataloaders
+# train_dataloader = 
+
+# Create Nerf Model
+coarse_nerf_model = NeRF_model()
+fine_nerf_model = NeRF_model()
+trainable_params = list(coarse_nerf_model.parameters()) + list(fine_nerf_model.parameters())
+
+#
+learning_rate = 5e-4
+
+# Define Optimizer
+optimizer = optim.Adam(params=trainable_params, lr=learning_rate, betas=(0.9, 0.999))
+
+# Include checkpointing and model loading code
+#
+
